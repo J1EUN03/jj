4d8da159b21a60d946ab0a73227173651acd3a99
J1EUN03 <153503193+J1EUN03@users.noreply.github.com>
2025-11-24 23:12:37 +0900

    Add files via upload

---
+++ 6.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int y = s.nextInt();
+        int z = y - 543;
+
+        System.out.println(z);
+    }
+}
